57ca0913a88ecd0de3b22a005624f52f616ceeb7
KyleMiles <krm504@nyu.edu>
2019-03-19 16:37:59 +0000

    Change Magic Numbers to Enums

--- components/BinaryNinjaContext.py
+++ components/BinaryNinjaContext.py
@@ -1,5 +1,6 @@
 from components.utils import drawList, drawMultiLineText
 import binaryninja as binja
+from enum import Enum
 import curses
 
 
@@ -16,6 +17,18 @@ import curses
 
 class BinaryNinjaContext():
   """ The BinaryNinja UI """
+
+  class View(Enum):
+    LINEAR = 0
+    HEX    = 1
+    CFG    = 2
+    GOTO   = 3
+
+  class Focus(Enum):
+    MAIN = 0
+    FUNC = 1
+    XREF = 2
+
   def __init__(self, program, screen, bv):
     self.program = program
     self.screen = screen
@@ -44,12 +57,9 @@ class BinaryNinjaContext():
     self.updateFunctionList = True
 
     # Global Stuff
-    self.focus = 0
-    self.view = 0
-    # 0 - Linear
-    # 1 - Hex
-    # 2 - CFG
-
+    self.focus = BinaryNinjaContext.Focus.MAIN
+    self.view = BinaryNinjaContext.View.LINEAR
+    
     # Set up first view
     self.linearDisassemblyScreen = curses.newpad(curses.LINES-1, curses.COLS - self.program.settings["functionListScreenWidth"])
     self.functionListScreen = curses.newpad(curses.LINES-1-self.program.settings["xrefsScreenHeight"], self.program.settings["functionListScreenWidth"])
@@ -163,11 +173,11 @@ class BinaryNinjaContext():
     if self.program.settings["BinaryNinjaContextDualFocus"]:
       self.parseInput_functionList(key)
       self.parseInput_linear_main(key)
-    elif self.focus == 0:
+    elif self.focus == BinaryNinjaContext.Focus.MAIN:
       self.parseInput_linear_main(key)
-    elif self.focus == 1:
+    elif self.focus == BinaryNinjaContext.Focus.FUNC:
       self.parseInput_functionList(key)
-    elif self.focus == 2:
+    elif self.focus == BinaryNinjaContext.Focus.XREF:
       self.parseInput_xrefs(key)
 
   def parseInput_hex(self, key):
@@ -263,64 +273,69 @@ class BinaryNinjaContext():
     if self.program.settings["BinaryNinjaContextDualFocus"]:
       self.parseInput_cfg_main(key)
       self.parseInput_functionList(key)
-    elif self.focus == 0:
+    elif self.focus == BinaryNinjaContext.Focus.MAIN:
       self.parseInput_cfg_main(key)
-    elif self.focus == 1:
+    elif self.focus == BinaryNinjaContext.Focus.FUNC:
       self.parseInput_functionList(key)
-    elif self.focus == 2:
+    elif self.focus == BinaryNinjaContext.Focus.XREF:
       self.parseInput_xrefs(key)
 
   def parseInput(self, key):
     if key == self.program.settings["BinaryNinjaContextSwitchView"]:
       # Clean Up
-      if self.view == 0:
+      if self.view == BinaryNinjaContext.View.LINEAR:
         del self.linearDisassemblyScreen
         del self.functionListScreen
         del self.xrefsScreen
-      elif self.view == 1:
+      elif self.view == BinaryNinjaContext.View.HEX:
         del self.hexScreen
-      elif self.view == 2:
+      elif self.view == BinaryNinjaContext.View.CFG:
         del self.cfgScreen
         del self.functionListScreen
         del self.xrefsScreen
 
       # Switch Contexts
-      if self.view == 0:
-        self.view = 1
-      elif self.view == 1:
-        self.view = 2
-      elif self.view == 2:
-        self.view = 0
+      if self.view == BinaryNinjaContext.View.LINEAR:
+        self.view = BinaryNinjaContext.View.HEX
+      elif self.view == BinaryNinjaContext.View.HEX:
+        self.view = BinaryNinjaContext.View.CFG
+      elif self.view == BinaryNinjaContext.View.CFG:
+        self.view = BinaryNinjaContext.View.LINEAR
 
       # Setup
-      if self.view == 0:
+      if self.view == BinaryNinjaContext.View.LINEAR:
         self.updateFunctionList = True
         self.linearDisassemblyScreen = curses.newpad(curses.LINES-1, curses.COLS - self.program.settings["functionListScreenWidth"])
         self.functionListScreen = curses.newpad(curses.LINES-1-self.program.settings["xrefsScreenHeight"], self.program.settings["functionListScreenWidth"])
         self.xrefsScreen = curses.newpad(self.program.settings["xrefsScreenHeight"], self.program.settings["functionListScreenWidth"])
         self.loadLinearDisassembly()
-      elif self.view == 1:
+      elif self.view == BinaryNinjaContext.View.HEX:
         self.updateFunctionList = True
         self.hexScreen = curses.newpad(curses.LINES-1, curses.COLS)
-      elif self.view == 2:
+      elif self.view == BinaryNinjaContext.View.CFG:
         self.cfgScreen = curses.newpad(curses.LINES-1, curses.COLS - self.program.settings["functionListScreenWidth"])
         self.functionListScreen = curses.newpad(curses.LINES-1-self.program.settings["xrefsScreenHeight"], self.program.settings["functionListScreenWidth"])
         self.xrefsScreen = curses.newpad(self.program.settings["xrefsScreenHeight"], self.program.settings["functionListScreenWidth"])
 
     elif key == self.program.settings["BinaryNinjaContextSwitchFocus"]:
-      if self.focus == 0:
-        self.focus = 1
-      elif self.focus == 1:
-        self.focus = 2
-      elif self.focus == 2:
-        self.focus = 0
+      if self.focus == BinaryNinjaContext.Focus.MAIN:
+        self.focus = BinaryNinjaContext.Focus.FUNC
+      elif self.focus == BinaryNinjaContext.Focus.FUNC:
+        self.focus = BinaryNinjaContext.Focus.XREF
+      elif self.focus == BinaryNinjaContext.Focus.XREF:
+        self.focus = BinaryNinjaContext.Focus.MAIN
 
-    if self.view == 0:
+    elif key == self.program.settings["BinaryNinjaContextSwitchFocus"]:
+      pass
+
+    if self.view == BinaryNinjaContext.View.LINEAR:
       self.parseInput_linear(key)
-    elif self.view == 1:
+    elif self.view == BinaryNinjaContext.View.HEX:
       self.parseInput_hex(key)
-    elif self.view == 2:
+    elif self.view == BinaryNinjaContext.View.CFG:
       self.parseInput_cfg(key)
+    elif self.view == BinaryNinjaContext.View.GOTO:
+      self.parseInput_goto(key)
 
   def render_functionList(self):
     # Not the best implimentation of this...but it makes only the functionList window input-lag (unless in dual focus mode... :/ )
@@ -328,7 +343,7 @@ class BinaryNinjaContext():
     if self.updateFunctionList:
       self.functionListScreen.erase()
 
-    if self.focus == 1 and not self.program.settings["BinaryNinjaContextDualFocus"]:
+    if self.focus == BinaryNinjaContext.Focus.FUNC and not self.program.settings["BinaryNinjaContextDualFocus"]:
       self.functionListScreen.border('#', '#', '#', '#', '#', '#', '#', '#')
     else:
       self.functionListScreen.border()
@@ -346,7 +361,7 @@ class BinaryNinjaContext():
 
   def render_xrefs(self):
     self.xrefsScreen.erase()
-    if self.focus == 2 and not self.program.settings["BinaryNinjaContextDualFocus"]:
+    if self.focus == BinaryNinjaContext.Focus.XREF and not self.program.settings["BinaryNinjaContextDualFocus"]:
       self.xrefsScreen.border('#', '#', '#', '#', '#', '#', '#', '#')
     else:
       self.xrefsScreen.border()
@@ -373,7 +388,7 @@ class BinaryNinjaContext():
     self.render_xrefs()
 
     self.linearDisassemblyScreen.erase()
-    if self.focus == 0 and not self.program.settings["BinaryNinjaContextDualFocus"]:
+    if self.focus == BinaryNinjaContext.Focus.MAIN and not self.program.settings["BinaryNinjaContextDualFocus"]:
       self.linearDisassemblyScreen.border('#', '#', '#', '#', '#', '#', '#', '#')
     else:
       self.linearDisassemblyScreen.border()
@@ -412,7 +427,7 @@ class BinaryNinjaContext():
     self.render_xrefs()
 
     self.cfgScreen.erase()
-    if self.focus == 0 and not self.program.settings["BinaryNinjaContextDualFocus"]:
+    if self.focus == BinaryNinjaContext.Focus.MAIN and not self.program.settings["BinaryNinjaContextDualFocus"]:
       self.cfgScreen.border('#', '#', '#', '#', '#', '#', '#', '#')
     else:
       self.cfgScreen.border()
@@ -439,11 +454,11 @@ class BinaryNinjaContext():
     self.alertsScreen.noutrefresh(0, 0, curses.LINES-1, 0, curses.LINES-1, curses.COLS-1)
 
   def render(self):
-    if self.view == 0:
+    if self.view == BinaryNinjaContext.View.LINEAR:
       self.render_linear()
-    elif self.view == 1:
+    elif self.view == BinaryNinjaContext.View.HEX:
       self.render_hex()
-    elif self.view == 2:
+    elif self.view == BinaryNinjaContext.View.CFG:
       self.render_cfg()
 
     self.render_alerts()
